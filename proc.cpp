4d4e6b2209ee14ee7054742d9a217db8042c1664
Zahu2018 <35234001+Zahu2018@users.noreply.github.com>
2019-09-24 13:45:16 +0300

    Add files via upload

---
+++ proc.cpp
@@ -0,0 +1,15 @@
+// In consola: g++ program.cpp
+// sau: g++ program.cpp -o prog
+// sau: g++ program.cpp -o prog.exe
+// gcc pentru programe C
+
+#include <iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main() {
+  cout << "ANSI OK";
+  getch();
+  return 0;
+  }
